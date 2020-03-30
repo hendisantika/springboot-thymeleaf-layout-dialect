dfa891062d1322c639381d2601535bc4696b22dc
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-31 06:58:51 +0700

    #8 - Add more mapping path in controller

--- src/main/java/com/hendisantika/springbootthymeleaflayoutdialect/HelloController.java
+++ src/main/java/com/hendisantika/springbootthymeleaflayoutdialect/HelloController.java
@@ -3,7 +3,9 @@ package com.hendisantika.springbootthymeleaflayoutdialect;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 
 /**
@@ -21,5 +23,20 @@ public class HelloController {
     public String index(Model model) {
         model.addAttribute("waktu", LocalDateTime.now());
         return "index";
+    }
+
+    @GetMapping(value = "/")
+    public ModelAndView test(HttpServletResponse response) {
+        return new ModelAndView("home");
+    }
+
+    @GetMapping(value = "/demoPage1")
+    public ModelAndView demoPage1(HttpServletResponse response) {
+        return new ModelAndView("demoPage1");
+    }
+
+    @GetMapping(value = "/demoPage2")
+    public ModelAndView demoPage2(HttpServletResponse response) {
+        return new ModelAndView("demoPage2");
     }
 }
