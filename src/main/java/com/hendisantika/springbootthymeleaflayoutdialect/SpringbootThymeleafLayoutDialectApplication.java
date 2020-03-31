8665dcc7af8fbdf6709b6b300c46b58c2444a77c
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-31 08:09:09 +0700

    #22 - Add Bean layoutDialect

--- src/main/java/com/hendisantika/springbootthymeleaflayoutdialect/SpringbootThymeleafLayoutDialectApplication.java
+++ src/main/java/com/hendisantika/springbootthymeleaflayoutdialect/SpringbootThymeleafLayoutDialectApplication.java
@@ -1,11 +1,16 @@
 package com.hendisantika.springbootthymeleaflayoutdialect;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootThymeleafLayoutDialectApplication extends SpringBootServletInitializer {
+public class SpringbootThymeleafLayoutDialectApplication {
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootThymeleafLayoutDialectApplication.class, args);
